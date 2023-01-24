959b7bc73d2b893b3ad2f24208b0415f4335e69e
Aleksandr Shevelev <leksevi@icloud.com>
2023-01-25 01:38:02 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите значения через запятую: ");
+string[] array = Console.ReadLine().Split(',');
+int count = 0;
+string[] temp = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        temp[i] = array[i];
+        count++;    
+    }
+}
+var result = temp.Where(s => !string.IsNullOrEmpty(s));
+Console.WriteLine(string.Join(", ", result));
